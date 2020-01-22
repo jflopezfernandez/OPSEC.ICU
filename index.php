e5b1ae3cad3cbdcf4446ec482e5057eaffc44b93
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-01-22 13:44:47 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<meta name="author" content="Jose Fernando Lopez Fernandez">
+
+	<title>OpSec.ICU</title>
+
+	<!-- CSS -->
+	<style>
+		html, body {
+			height: 100%;
+		}
+
+		main {
+			min-height: 100%;
+		}
+
+		.container {
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			text-align: center;
+		}
+	</style>
+	<!-- Javascript -->
+</head>
+<body>
+	<main role="main" style="height: 100%;">
+		<div class="container">
+			<h1>Under Construction</h1>
+		</div>
+	</main>
+</body>
+</html>
